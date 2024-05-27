1c793cdb59fc94fb1065fd010ad6606347e5a6cb
helltractor <1664309610@qq.com>
2024-05-27 14:10:45 +0800

    Upload code in the past week

---
+++ Contests/codeforces/Daily/1400/1929C.py
@@ -0,0 +1,59 @@
+ImportType = 1
+InputType = 1
+ConstType = 1
+if ImportType:
+    import os, sys, random, threading
+    from random import randint, choice, shuffle
+    from copy import deepcopy
+    from io import BytesIO, IOBase
+    from types import GeneratorType
+    from functools import lru_cache, reduce
+    from bisect import bisect_left, bisect_right
+    from collections import Counter, defaultdict, deque
+    from itertools import accumulate, combinations, permutations
+    from heapq import heapify, heappop, heappush, heappushpop
+    from typing import Generic, Iterable, Iterator, TypeVar, Union, List
+    from string import ascii_lowercase, ascii_uppercase, digits
+    from math import ceil, comb, floor, sqrt, pi, factorial, gcd, log, log10, log2, inf
+    from decimal import Decimal, getcontext
+    from sys import stdin, stdout, setrecursionlimit
+
+if InputType:
+    input = lambda: sys.stdin.readline().rstrip("\r\n")
+    I = lambda: input()
+    II = lambda: int(input())
+    MII = lambda: map(int, input().split())
+    LI = lambda: list(input().split())
+    LII = lambda: list(map(int, input().split()))
+    GMI = lambda: map(lambda x: int(x) - 1, input().split())
+    LGMI = lambda: list(map(lambda x: int(x) - 1, input().split()))
+
+if ConstType:
+    RD = random.randint(10 ** 9, 2 * 10 ** 9)
+    MOD = 998244353
+    Y = "Yes"
+    N = "No"
+
+
+def cf_1929C():
+    for _ in range(II()):
+        k, x, a = MII()
+        base = 1
+        for _ in range(x - 1):
+            y = ceil(base / (k - 1)) + int(base % (k - 1) == 0)
+            base += y
+            # print(base)
+        if (a - base) * (k - 1) > base:
+            print("YES")
+        else:
+            print("NO")
+    return
+
+
+def main():
+    cf_1929C()
+    return
+
+
+if __name__ == '__main__':
+    main()
